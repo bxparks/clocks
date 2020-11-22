15884f2aaff8b3ae184d71024a411609dc260e60
Brian Park <brian@xparks.net>
2020-11-21 18:15:25 -0800

    WorldClock: Add version strings for AceButton and AceRoutine, having 450 bytes to spare

--- WorldClock/Presenter.cpp
+++ WorldClock/Presenter.cpp
@@ -1,3 +1,4 @@
+#include <AceTime.h>
 #include <AceButton.h>
 #include <AceRoutine.h>
 #include "Presenter.h"
@@ -6,11 +7,14 @@ void Presenter::displayAbout() const {
   mOled.set1X();
 
   // For smallest flash memory size, use F() macros for these longer strings,
-  // but no F() for shorter version strings. Comment out the headers, to save
-  // 38 bytes because I'm 12 bytes over the 30kB limit of a Pro Micro.
+  // but no F() for shorter version strings.
 
-  //mOled.print(F("AT: "));
-  mOled.println(F(ACE_TIME_VERSION_STRING));
-  //mOled.print(F("TZ: "));
+  mOled.print(F("TZ: "));
   mOled.println(zonedb::kTzDatabaseVersion);
+  mOled.print(F("AT: "));
+  mOled.println(F(ACE_TIME_VERSION_STRING));
+  mOled.print(F("AB: "));
+  mOled.println(F(ACE_BUTTON_VERSION_STRING));
+  mOled.print(F("AR: "));
+  mOled.println(F(ACE_ROUTINE_VERSION_STRING));
 }
