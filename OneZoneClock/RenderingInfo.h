5dc1170de4b87798d65a7fe47320fcef26227c75
Brian Park <brian@xparks.net>
2021-03-22 13:00:27 -0700

    OneZoneClock: Move ClockInfo directly into RenderingInfo; use random contextId

--- OneZoneClock/RenderingInfo.h
+++ OneZoneClock/RenderingInfo.h
@@ -2,7 +2,8 @@
 #define ONE_ZONE_CLOCK_RENDERING_INFO_H
 
 #include <AceTime.h>
-#include "config.h"
+#include "config.h" // MODE_UNKNOWN
+#include "ClockInfo.h"
 
 /**
  * Data used by the Presenter (the "View") to determine what has changed and
@@ -12,35 +13,14 @@ struct RenderingInfo {
   uint8_t mode = MODE_UNKNOWN; // display mode, see MODE_xxx in config.h
   bool blinkShowState; // true if blinking info should be shown
 
-  uint8_t hourMode; // ClockInfo::kTwelve or kTwentyFour
-
-  #if DISPLAY_TYPE == DISPLAY_TYPE_LCD
-    uint8_t backlightLevel; // LCD backlight level [0, 9] -> PWM value
-    uint8_t contrast; // LCD contrast [0, 127]
-    uint8_t bias; // LCD backlight level [0, 7]
-  #else
-    uint8_t contrastLevel; // OLED contrast level [0, 9] -> [0, 255]
-    uint8_t invertDisplay; // 0: off, 1: on; should never be 2
-  #endif
-
-  ace_time::TimeZoneData timeZoneData;
-  ace_time::ZonedDateTime dateTime;
+  ClockInfo clockInfo;
 };
 
 inline bool operator==(const RenderingInfo& a, const RenderingInfo& b) {
-  return a.mode == b.mode
-      && a.blinkShowState == b.blinkShowState
-      && a.hourMode == b.hourMode
-    #if DISPLAY_TYPE == DISPLAY_TYPE_LCD
-      && a.backlightLevel == b.backlightLevel
-      && a.contrast == b.contrast
-      && a.bias == b.bias
-    #else
-      && a.contrastLevel == b.contrastLevel
-      && a.invertDisplay == b.invertDisplay
-    #endif
-      && a.timeZoneData == b.timeZoneData
-      && a.dateTime == b.dateTime;
+  return
+    a.blinkShowState == b.blinkShowState
+    && a.mode == b.mode
+    && a.clockInfo == b.clockInfo;
 }
 
 inline bool operator!=(const RenderingInfo& a, const RenderingInfo& b) {
